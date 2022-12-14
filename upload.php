93c213a72bda74be76dff9c06f1d2d2012b9419d
Mmed Amin Mesmoudi <m.mesmoudi@esi-sba.dz>
2022-12-14 11:59:02 +0100

    styling

--- upload.php
+++ upload.php
@@ -8,50 +8,50 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
   if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
+    echo "<h1><center>File is an image </center></h1>" ;
 
     $uploadOk = 1;
   } else {
-    echo "<br>File is not an image.";
+    echo "<br><h1><center>File is not an image.</center></h1>";
     $uploadOk = 0;
   }
 }
 
 // Check if file already exists
 if (file_exists($target_file)) {
-  echo "<br>Sorry, file already exists.";
+  echo "<br><h1><center>Sorry, file already exists.</center></h1>";
   $uploadOk = 0;
 }
 
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
-  echo "<br>Sorry, your file is too large.";
+  echo "<br><h1><center>Sorry, your file is too large.</center></h1>";
   $uploadOk = 0;
 }
 
 //Allow certain file formats
 if($imageFileType != "jpg" ) {
-  echo "<br>Sorry, only JPG is allowed.";
+  echo "<br><h1><center>Sorry, only JPG is allowed.</center></h1>";
   $uploadOk = 0;
 }
 
 if (substr_count($_FILES["fileToUpload"]["name"], '.') > 1){
-    echo "<br>Double extension";
+    echo "<br><h1><center>Double extension</center></h1>";
     $uploadOk = 0;
 }
 
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-  echo "<br>Sorry, your file was not uploaded.";
+  echo "<br><h1><center>Sorry, your file was not uploaded.</center></h1>";
 
 
 
 // if everything is ok, try to upload file
 } else {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "<br>The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+    echo "<br><h1><center>The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.</center></h1>";
   } else {
-    echo "<br>Sorry, there was an error uploading your file.";
+    echo "<br><h1><center>Sorry, there was an error uploading your file.</center></h1>";
   }
 }
 ?>
